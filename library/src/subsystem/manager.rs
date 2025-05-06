67f4bb6b10396203058c0213a4e5661752a16c2f
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2025-05-05 19:36:21 -0700

    use a lock
    this seems like it could fix some potential race conditions

--- library/src/subsystem/manager.rs
+++ library/src/subsystem/manager.rs
@@ -1,47 +1,65 @@
 // todo: is a mutex needed here
+// todo: is an arc needed here
+use std::sync::{Arc, RwLock, RwLockWriteGuard};
 
 use once_cell::sync::Lazy;
 
-use super::{StrongOpaque, Subsystem, SubsystemRaw, SubsystemTrait, WeakOpaque};
+use super::{StrongOpaque, Subsystem, SubsystemManaged, SubsystemRaw, SubsystemTrait, WeakOpaque};
 static TRACKER: Lazy<SubsystemManager> = Lazy::new(|| SubsystemManager::new());
 pub struct SubsystemManager {
-    subsystems: Vec<WeakOpaque>,
+    subsystems: RwLock<Vec<WeakOpaque>>,
 }
 impl SubsystemManager {
     pub fn new() -> Self {
-        Self { subsystems: vec![] }
+        Self {
+            subsystems: RwLock::new(vec![]),
+        }
+    }
+    pub fn get_subsystems(&mut self) -> RwLockWriteGuard<'_, Vec<WeakOpaque>> {
+        self.subsystems.write().unwrap()
     }
     pub fn add_subsystem<T: SubsystemTrait>(&mut self, subsystem: Subsystem<T>) {
-        self.subsystems.push(subsystem.as_opaque_weak());
-    }
-    pub fn get_subsystems(&mut self) -> &mut Vec<WeakOpaque> {
-        &mut self.subsystems
+        self.get_subsystems().push(subsystem.as_opaque_weak());
     }
     pub fn get_subsystems_by_type<T: SubsystemTrait + 'static>(
         &mut self,
-    ) -> impl Iterator<Item = StrongOpaque> + '_ {
-        self.get_subsystems()
-            .into_iter()
-            .filter_map(|sub| {
-                sub.upgrade().and_then(|strong| {
-                    let inner = &strong.read().ok()?.inner;
-                    if inner.inner.is::<T>() {
-                        Some(strong.clone())
-                    } else {
-                        None
-                    }
-                })
+    ) -> impl Iterator<Item = StrongOpaque> {
+        self.get_subsystems().clone().into_iter().filter_map(|sub| {
+            sub.upgrade().and_then(|strong| {
+                let inner = &strong.read().ok()?.inner;
+                if inner.inner.is::<T>() {
+                    Some(strong.clone())
+                } else {
+                    None
+                }
             })
+        })
     }
-    pub fn execute_all_generic(&mut self, run: impl FnMut(&mut SubsystemRaw) -> ()) {
-    }
-    pub fn periodic_all() {
 
+    pub fn execute_all_generic(&mut self, mut run: impl FnMut(&mut SubsystemRaw)) {
+        self.get_subsystems().clone().retain(|weak| {
+            if let Some(strong) = weak.upgrade() {
+                if let Ok(mut guard) = strong.write() {
+                    run(&mut guard.inner);
+                }
+                true
+            } else {
+                false // drop dead weak
+            }
+        });
     }
-    pub fn write_packet_all() {
-
+    pub fn periodic_all() {}
+    pub fn write_packet_all() {}
+    pub fn read_packet_all() {}
+    pub fn remove_dropped(&mut self) {
+        self.get_subsystems()
+            .retain(|weak| weak.upgrade().is_some());
     }
-    pub fn read_packet_all() {
-
+    pub fn approximate_len(&mut self) -> usize {
+        self.get_subsystems().len()
+    }
+    pub fn len(&mut self) -> usize {
+        // self.subsystems.iter().count()
+        0
     }
 }
