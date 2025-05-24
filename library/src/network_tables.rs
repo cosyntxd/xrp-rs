3ecc446de96a44946d85a7d7d0b35513c4b0f11a
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2025-05-23 23:29:15 -0700

    working networktable library
    the other one i was trying to use only supported up to 4.0 and not 4.1 however this one should match whatever wpilib cpp is using

--- library/src/network_tables.rs
+++ library/src/network_tables.rs
@@ -1,52 +1,49 @@
-// few people are crazy enough to implement a working server
+use std::sync::Mutex;
 
-// pub struct NetworkTable {
-    
-// }
-// impl NetworkTable {
-//     pub fn bind(addr: &str) -> Self {
-//         Self {}
-//     }
-//     pub fn set(&mut self, key: &str, value: u8) {
+use nt_rs::{instance::NetworkTableInstance, nt};
+use once_cell::sync::Lazy;
+// pub use nt_rs::ins
+static INSTANCE: Lazy<Mutex<NetworkTableInstance>> = Lazy::new(|| {
+    let mut instance = NetworkTableInstance::default();
+    instance.start_server(Default::default());
+    Mutex::new(instance)
+});
 
-//     }
-//     pub fn get(&mut self, key: &str, value: u8) {
 
-//     }
-// }
+#[macro_export]
+macro_rules! nt_bind {
+    (bind $addr:expr) => {{
 
-use frclib_core::value::IntoFrcValue;
-use std::{net::SocketAddr, ops::{Deref, DerefMut}};
-use nt_table::{client, server::BlockingServerHandle, server::config::ServerConfig};
-use std::net::{ToSocketAddrs};
-
-pub struct NetworkTable {
-    table: BlockingServerHandle,
+    }}
 }
 
-impl NetworkTable {
-    pub fn bind(addr: &str) -> Self {
-        let server_addr = addr
-            .to_socket_addrs()
-            .unwrap()
-            .next()
-            .expect("Invalid address");
-        Self {
-            table: BlockingServerHandle::start(server_addr, ServerConfig::default()).unwrap(),
-        }
+#[macro_export]
+macro_rules! nt_write {
+    ($path:expr, $val:expr) => {{
+
+    }};
+}
+
+#[macro_export]
+macro_rules! nt_read {
+    ($path:expr) => {{
+
+    }};
+}
+
+
+pub fn is_bound() -> bool {
+    unsafe { BOUND }
+}
+
+pub fn mark_bound() {
+    unsafe {
+        BOUND = true;
     }
 }
 
-impl Deref for NetworkTable {
-    type Target = BlockingServerHandle;
-
-    fn deref(&self) -> &Self::Target {
-        &self.table
-    }
-}
-
-impl DerefMut for NetworkTable {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.table
-    }
+pub fn bind_default() {
+    let mut instance = get_instance();
+    instance.start_server(Default::default());
+    mark_bound();
 }
--- robot/src/main.rs
+++ robot/src/main.rs
@@ -1,9 +1,7 @@
 use std::{net::SocketAddr, thread};
 
-use xrp::{frc::motor::Motor, subsystem::Subsystem, subsystem::manager::SubsystemManager, network_tables::NetworkTable};
-use nt::{EntryData, NetworkTables};
+use xrp::{frc::motor::Motor, subsystem::Subsystem, subsystem::manager::SubsystemManager};
 fn main() {
-    let mut nt_table = NetworkTable::bind("127.0.0.1:5810");
     let mut a = Subsystem::new(Motor::new(0));
     let mut b = Subsystem::new(Motor::new(1));
     a.depends_on(&b);
@@ -15,16 +13,9 @@ fn main() {
     
     SubsystemManager::tracker().periodic_all();
     assert!(SubsystemManager::tracker().len() == 2);
-    // let mut table = nt_table::server::BlockingServerHandle::start(SocketAddr, config)
-    // let mut nt = NetworkTables::bind("127.0.0.1:5810", "nt-rs-server");
-    // let a = pollster::block_on(nt.create_entry(EntryData::new("yoooooo".to_owned(), 0, nt::EntryValue::Boolean(false)))).unwrap();
-    nt_table.publish_topic("aa", 1);
-    
 
     loop {
         thread::sleep_ms(1);
-
-        println!("{:?}", nt_table.get_topic_value(&"aa".to_owned()).unwrap());
-    //     nt.update_entry(a, nt::EntryValue::Double(0.0));
     }
 }
+// that video with eveyrthig about chem/ physics
